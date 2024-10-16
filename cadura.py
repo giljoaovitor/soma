731eb20ca472f2c6f2ff9101bf1cd2dbd2fb8d96
giljoaovitor <joao.vg19@aluno.ifsc.edu.br>
2024-10-16 12:34:39 -0300

    vetor

--- cadura.py
+++ cadura.py
@@ -22,7 +22,12 @@ import array as ar
 def forma3_leitura_array():
     vet = ar.array('i', map(int, input().split()))
     tamanho_vet = len(vet)
-
+    for i in range(tamanho_vet):
+        #vet[i] = 3*vet[i]
+        vet[i]*=3
+        print(vet[i], end = ' ')
+    print()
+    
 if __name__=="__main__":
     #forma1_leitura()
     #forma2_leitura()
